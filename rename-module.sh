baf87bc9e32a6d271a841f0a24c5ba986dea6da5
Andreas Palm <andreaspalm98@gmail.com>
2024-08-22 10:28:16 +0200

    fix(rename-module): Not using the module path in sed

--- rename-module.sh
+++ rename-module.sh
@@ -8,4 +8,4 @@ if [[ "$modulePath" == "" ]]; then
     exit 1
 fi
 
-find . -type f -not -path '*/\.git/*' -not -name 'rename-module.sh' -exec sed -i 's+github.com/zapling/go-service+github.com/zapling/something-else+g' '{}' +
+find . -type f -not -path '*/\.git/*' -not -name 'rename-module.sh' -exec sed -i "s+github.com/zapling/go-service+$modulePath+g" '{}' +
