1331f839365ecf04ff161a4803c1d972a9885e9a
Andreas Palm <andreaspalm98@gmail.com>
2024-08-22 10:20:15 +0200

    fix(rename-module.sh): Require module name argument

--- rename-module.sh
+++ rename-module.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
+modulePath=$1
+if [[ "$modulePath" == "" ]]; then
+    echo "You must provide a module path as argument"
+    printf "\nExample:\n"
+    echo "./rename-module.sh github.com/zapling/some-go-service"
+    exit 1
+fi
+
 find . -type f -not -path '*/\.git/*' -not -name 'rename-module.sh' -exec sed -i 's+github.com/zapling/go-service+github.com/zapling/something-else+g' '{}' +
